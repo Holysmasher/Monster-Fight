cf7fc345d015a2da9fa97470d739c3206dee7fb9
Holysmasher <shawnsummers@live.com>
2026-01-13 16:08:27 -0700

    Created Monster class

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import random
+
+
+class Monster:
+    def __init__(self, name, max_health, attack, type):
+        self.name = name
+        self.__max_health = max_health
+        self.__health = max_health
+        self.__attack = attack
+        self.__type = type
+
 
 def main():
     pass
+
+
 if __name__ == '__main__':
     main()
